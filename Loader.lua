4a34711b2f02ea9144f712c4a7260e69a9087035
Ajhohack <49101276+Ajhohack@users.noreply.github.com>
2025-08-23 04:40:41 +0700

    Add files via upload

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,4 @@
-local MainUIScript = game:HttpGet("https://raw.githubusercontent.com/armiid24/Aikoo-X-Lua/refs/heads/main/MainUIFrame.lua")
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Ajhohack/Aikoo-X-Lua/refs/heads/main/Loader.lua"))() --
+
+local MainUIScript = game:HttpGet("https://raw.githubusercontent.com/Ajhohack/Aikoo-X-Lua/refs/heads/main/MainUIFrame.lua")
 loadstring(MainUIScript)()
